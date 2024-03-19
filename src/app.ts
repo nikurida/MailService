2db733d5f0e33112bc39280a20bb6322b74f0d4e
Ricardo <rikurida@gmail.com>
2024-03-19 17:06:53 -0400

    add bull board

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes';
 import dotenv from 'dotenv';
+import { BullAdapter } from 'bull-board/bullAdapter';
+import { createBullBoard } from 'bull-board';
+import queue from './config/queueConfig';
 
 const app = express();
+
+const { router: bullBoardRouter } = createBullBoard([
+    new BullAdapter(queue)
+]);
 
 app.use(cors());
 
@@ -17,6 +24,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use('/api', router);
+app.use('/admin/queues', bullBoardRouter);
 
 app.listen(4000, () => {
     console.log('Server running on http://localhost:4000');
